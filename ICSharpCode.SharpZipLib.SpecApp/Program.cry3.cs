fb64dd1f81087c7f7b6837bcc445fccac46cc28a
Sky Morey <smorey@degdigital.com>
2022-04-10 00:13:27 -0500

    published

--- ICSharpCode.SharpZipLib.SpecApp/Program.cry3.cs
+++ ICSharpCode.SharpZipLib.SpecApp/Program.cry3.cs
@@ -2,8 +2,9 @@
 using System.Text;
 
 //None(OK)
-//const string PakPath = @"D:\Program Files (x86)\Steam\steamapps\common\Wolcen\Game\Textures_decals.pak";
-//const string AesKey = null; // @"aes:30818902818100E2725EF9BB168871C238D91B64CFB8B1332F1BBCF105F40F252FB93F3A609D524CF8F5EE09BC554FD918DB8BB3531D6F88BEFEA4BFBDF51CB1E1DF5E5DFA83FD6584D37E279924224FC4F8BB6C98ED50D27002E8BA21F35F0155A08D9ED276714032AEECDA066C17FA54F1C33E5DAF8B332B3CC0771490A15261B2DD908F53F10203010001";
+const string PakPath = @"D:\Program Files (x86)\Steam\steamapps\common\Wolcen\Game\Textures_decals.pak";
+//const string PakPath = @"D:\Program Files (x86)\Steam\steamapps\common\Wolcen\Game\Sounds_WEM.pak";
+const string AesKey = null; // @"aes:30818902818100E2725EF9BB168871C238D91B64CFB8B1332F1BBCF105F40F252FB93F3A609D524CF8F5EE09BC554FD918DB8BB3531D6F88BEFEA4BFBDF51CB1E1DF5E5DFA83FD6584D37E279924224FC4F8BB6C98ED50D27002E8BA21F35F0155A08D9ED276714032AEECDA066C17FA54F1C33E5DAF8B332B3CC0771490A15261B2DD908F53F10203010001";
 
 // TEA, comments:TEA (ERROR)
 //const string PakPath = @"D:\Program Files (x86)\Steam\steamapps\common\Warface\13_2000076\Game\GameInfo.pak";
@@ -11,23 +12,27 @@ using System.Text;
 
 // comments:NEWHUNT|NEWHUNT (OK)
 //const string PakPath = @"D:\Program Files (x86)\Steam\steamapps\common\Hunt Showdown\game_hunt\gamedata.pak";
-const string PakPath = @"D:\Program Files (x86)\Steam\steamapps\common\Hunt Showdown\game_hunt\audio.pak";
-const string AesKey = "aes:30818902818100affd71ca741c1aa5895becf596e8732d290453d275cf6ff0bb214324ebab7eedd7f39deebc2708d88b6d536a58da5683137fafec478e41e6f8b0882e5eba236b9d2a150ee513ae562ce56b6aaf982c27a8c317281afa0f84f546ecb825ccf2217519c84ed0ceab179ee5ccdab0cb40a95d5442120f25a61e7da79d30c7d7d8a70203010001";
+//const string PakPath = @"D:\Program Files (x86)\Steam\steamapps\common\Hunt Showdown\game_hunt\audio.pak";
+//const string AesKey = "aes:30818902818100affd71ca741c1aa5895becf596e8732d290453d275cf6ff0bb214324ebab7eedd7f39deebc2708d88b6d536a58da5683137fafec478e41e6f8b0882e5eba236b9d2a150ee513ae562ce56b6aaf982c27a8c317281afa0f84f546ecb825ccf2217519c84ed0ceab179ee5ccdab0cb40a95d5442120f25a61e7da79d30c7d7d8a70203010001";
 
 // comments:STREAMCIPHER_KEYTABLE|CDR_SIGNED (OK)
 //const string PakPath = @"D:\Program Files (x86)\Steam\steamapps\common\SNOW\Assets\GameData.pak";
+//const string PakPath = @"D:\Program Files (x86)\Steam\steamapps\common\SNOW\Assets\Sounds.pak";
 //const string AesKey = "aes:30818902818100D51E1D3810C4A112B2F2504B83E2F124009C0AC9CD1661913421D4E94623AD7014599DAFB0DC9F8366D164AD072B3DC5AA3D4CD24542D5F684E6A4F7473102DE2ACA11F6524015ECBD564248FC712B3A69B15B78EFAA06748259DDE77A75757E513F7AC21A0151F53C78FF45ABCC45C3F54BC6305F420981F7119AF03E6438D70203010001";
 
 // TEA (OK)
 //const string PakPath = @"D:\Program Files (x86)\Steam\steamapps\common\Ryse Son of Rome\GameRyse\GameData.pak";
+//const string PakPath = @"D:\Program Files (x86)\Steam\steamapps\common\Ryse Son of Rome\GameRyse\Music.pak";
 //const string AesKey = null;
 
 // comments:CDR_SIGNED (OK)
 //const string PakPath = @"D:\Program Files (x86)\Steam\steamapps\common\Robinson The Journey\game_robinson\gamedata.pak";
+//const string PakPath = @"D:\Program Files (x86)\Steam\steamapps\common\Robinson The Journey\game_robinson\audio.pak";
 //const string AesKey = null;
 
 // None (OK)
 //const string PakPath = @"D:\Program Files (x86)\Steam\steamapps\common\Crysis Remastered\Game\gamedata.pak";
+//const string PakPath = @"D:\Program Files (x86)\Steam\steamapps\common\Crysis Remastered\Game\music.pak";
 //const string AesKey = null;
 
 using var pakStream = File.Open(PakPath, FileMode.Open, FileAccess.Read, FileShare.Read);
@@ -35,7 +40,7 @@ var pak = new Cry3File(pakStream, Utils.ParseKey(AesKey));
 foreach (ZipEntry ent in pak)
 {
     Console.WriteLine($"{ent.Name} - {ent.CompressionMethod}");
-    
+
     if (false) continue;
 
     // read
@@ -43,7 +48,7 @@ foreach (ZipEntry ent in pak)
     {
         // create directory
         using var input = pak.GetInputStream(ent);
-        var newPath = Path.Combine(@"C:\T_\game_hunt\", ent.Name);
+        var newPath = Path.Combine(@"C:\T_\Crysis\", ent.Name);
         var directory = Path.GetDirectoryName(newPath);
         if (!string.IsNullOrEmpty(directory) && !Directory.Exists(directory)) Directory.CreateDirectory(directory);
         using var s = new FileStream(newPath, FileMode.Create, FileAccess.Write);
